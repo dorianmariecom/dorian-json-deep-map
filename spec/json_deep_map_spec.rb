f4a1cd7f8ef7c741c18edca72bd85d759c64ae33
Dorian Marié <dorian@dorianmarie.com>
2024-08-23 22:22:31 +0200

    add deep_map function and improve argument parsing

--- spec/json_deep_map_spec.rb
+++ spec/json_deep_map_spec.rb
@@ -2,5 +2,16 @@
 
 require "spec_helper"
 
-RSpec.describe "each" do
+RSpec.describe "json-deep-map" do
+  it "works" do
+    expect(
+      `echo [1,2,3] | bin/json-deep-map "it = it + 1 if it.is_a?(Integer)"`
+    ).to eq(<<~OUTPUT)
+    [
+      2,
+      3,
+      4
+    ]
+    OUTPUT
+  end
 end
