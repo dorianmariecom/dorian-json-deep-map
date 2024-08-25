1e33fc9c42327348972c5dfe9db7db10989290d7
Dorian Marié <dorian@dorianmarie.com>
2024-08-25 20:09:52 +0200

    update gem dependencies and configuration files

--- spec/json_deep_map_spec.rb
+++ spec/json_deep_map_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe "json-deep-map" do
     expect(
       `echo [1,2,3] | bin/json-deep-map "it = it + 1 if it.is_a?(Integer)"`
     ).to eq(<<~OUTPUT)
-    [
-      2,
-      3,
-      4
-    ]
+      [
+        2,
+        3,
+        4
+      ]
     OUTPUT
   end
 end
